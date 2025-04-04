ae7fceb91df646778957d838e78a485869405fca
shreeja-sundaramoorthy <shubhyshree17@gmail.com>
2025-04-04 16:11:58 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="🏠",
+)
+
+st.title("Local Food Waste Management")
+
+
+st.image("project_1titlepic.jpg",width=650)
+
+st.markdown("""Food waste is a major worldwide problem that affects society, the economy, and the environment. The waste of perfectly good food exposes inefficiencies in production, distribution, and consumption, wastes resources like water and energy, and adds to greenhouse gas emissions in landfills. Remarkably, tonnes of food go uneaten every year, and millions of people suffer from hunger.
+            
+One effective strategy to cut waste and assist those in need is to manage extra food locally.           
+            """)
